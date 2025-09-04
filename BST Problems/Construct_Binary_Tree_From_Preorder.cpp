e0b34f2ac000e5d96729bfdd32ad0b4c779c26d1
Vector-mayu <mayureshdandekar143@gmail.com>
2025-09-04 23:58:18 +0530

    Added BST using Preorder

---
+++ BST Problems/Construct_Binary_Tree_From_Preorder.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+    TreeNode* BST(vector<int>&preorder, int &index, int lower, int upper)
+    {
+        //base Conditon 
+        if(index >= preorder.size() || preorder[index]<lower || preorder[index]>upper)
+        return NULL;
+
+        //Node
+        TreeNode *root = new TreeNode(preorder[index++]);
+        //Left
+        root->left = BST(preorder, index, lower, root->val);
+        //Right
+        root->right = BST(preorder,index,root->val, upper);
+
+        return root;
+    }
+
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        int index = 0; // Iterator
+        return BST(preorder, index, INT_MIN, INT_MAX);
+    }
+};
