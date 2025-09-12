7889d989ba46d314017c44003a604132c7bc4087
Vector-mayu <mayureshdandekar143@gmail.com>
2025-09-12 23:57:38 +0530

    Added Minimum Distance Btwn BST Nodes

---
+++ BST Problems/Minimum_Distance_Between_BST_Nodes.cpp
@@ -0,0 +1,25 @@
+class Solution {
+
+    void minDist(TreeNode *root, int &prev, int &ans)
+    {
+        if(!root)
+        return;
+
+        minDist(root->left, prev, ans);
+
+        if(prev != INT_MIN)
+        ans = min(ans, root->val - prev);
+        prev = root->val;
+
+        minDist(root->right, prev, ans);
+    }
+
+public:
+    int minDiffInBST(TreeNode* root) {
+
+        int prev = INT_MIN;
+        int ans = INT_MAX;
+        minDist(root, prev, ans);
+        return ans;
+    }
+};
