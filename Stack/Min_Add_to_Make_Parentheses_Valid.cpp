7b089434e07a05aae4fcb7f12ba0d1ac1dd870cd
Vector-mayu <mayureshdandekar143@gmail.com>
2025-10-09 23:58:39 +0530

    Added LC Problem min add to make parentheses valid

---
+++ Stack/Min_Add_to_Make_Parentheses_Valid.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+
+        stack<int>st;
+        int count = 0;
+        for(int i=0; i<s.size();i++)
+        {
+            //opening
+            if(s[i]=='(')
+            {
+                st.push(s[i]);
+            }
+            //closing
+            else
+            {
+                if(st.empty())
+                {
+                    count++;
+                }
+                else
+                {
+                    st.pop();
+                }
+            }
+        }
+
+        return count + st.size();
+
+    }
+};
