79f4c4604cb35468028cda2bd6d4c3a132be29c9
Vector-mayu <mayureshdandekar143@gmail.com>
2025-10-13 23:59:37 +0530

    Added Queue

---
+++ Queue/d.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+
+        int time=0;
+        for(int i=0; i<=k; i++)
+        {
+            time+= min(tickets[k], tickets[i]);
+        }
+        for(int i=k+1; i<tickets.size(); i++)
+        {
+            time+= min(tickets[k]-1, tickets[i]);
+        }
+
+        return time;
+        
+    }
+};
