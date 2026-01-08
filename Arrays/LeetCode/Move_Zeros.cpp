bec87100e42e12032a853cd94ab1187309a0aec3
Vector-mayu <mayureshdandekar143@gmail.com>
2026-01-08 18:48:18 +0530

    Added LC Move Zeros Q

---
+++ Arrays/LeetCode/Move_Zeros.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+        int j = 0;
+
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i] != 0){
+                nums[j++] = nums[i];  
+            }
+        }
+
+        for(int i=j; i<n;i++)
+        {
+            nums[i] = 0;
+        }
+
+
+
+    }
+};
