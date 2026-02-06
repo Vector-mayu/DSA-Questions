5677e9c2d8ccb72039fe5c69cf3882f29725a1db
Vector-mayu <mayureshdandekar143@gmail.com>
2026-02-06 23:59:03 +0530

    Added Weighted Graph Adjacency Matrix Code

--- Graphs/Adjacency_Matrix.cpp
+++ Graphs/Adjacency_Matrix.cpp
@@ -1,3 +1,5 @@
+// UnWeighted Graph
+
 #include<iostream>
 #include<vector>
 using namespace std;
@@ -15,6 +17,35 @@ int main()
         cin>>u>>v;
         AdjMat[u][v] = 1;
         AdjMat[v][u] = 1;
+    }
+    
+    for(int i=0; i<vertex; i++){
+        for(int j=0; j<vertex; j++){
+            cout<<AdjMat[i][j]<<" ";
+        }
+		cout<<endl;
+    }
+}
+
+// Weighted Graph
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//Adjacency Matrix
+int main()
+{
+    int vertex , edges;
+    cin>>vertex>>edges;
+    
+    vector<vector<bool> >AdjMat(vertex, vector<bool>(vertex, 0));
+    
+    int u, v, val;
+    for(int i=0; i<edges; i++){
+        cin>>u>>v>>val;
+        AdjMat[u][v] = val;
+        AdjMat[v][u] = val;
     }
     
     for(int i=0; i<vertex; i++){
