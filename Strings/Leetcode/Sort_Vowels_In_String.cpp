28799eea95b482de8b7cb35395b9d42dbb9f9d98
Vector-mayu <mayureshdandekar143@gmail.com>
2026-03-22 12:53:07 +0530

    Solved LC Sort Vowels in a String Question

---
+++ Strings/Leetcode/Sort_Vowels_In_String.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string sortVowels(string s) {
+        
+        string temp;
+        for(int i=0; i<s.length(); i++){
+            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u' || s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'){
+                temp.push_back(s[i]);
+            }
+        }
+
+        sort(temp.begin(), temp.end());
+
+        int j=0;
+        for(int i=0; i<s.length(); i++){
+            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u' || s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'){
+                s[i] = temp[j];
+                j++;
+            }
+        }
+
+        return s;
+    }
+};
