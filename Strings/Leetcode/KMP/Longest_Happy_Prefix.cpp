c5320a1d4f5fb7e04375b8f3d1b0b962165ba5e5
Vector-mayu <mayureshdandekar143@gmail.com>
2026-03-31 10:11:47 +0530

    Solved LC 1392 Longest Happy String Question

---
+++ Strings/Leetcode/KMP/Longest_Happy_Prefix.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string longestPrefix(string s) {
+        int n = s.length();
+        
+        // LPS array
+        vector<int>LPS(n,0);
+        
+        int prefix = 0, suffix = 1;
+        
+        while(suffix < n){
+            // If both are equal 
+            if(s[prefix] == s[suffix]){
+                LPS[suffix] = prefix + 1;
+                suffix++, prefix++;
+            }
+            // if they are not equal
+            else{
+                // if its 0th position of prefix 
+                if(prefix == 0)
+                suffix++;
+                // go where str1 and str2 has common matches 
+                else{
+                    prefix = LPS[prefix - 1];
+                }  
+            }
+        }
+        
+        return s.substr(0, LPS[n-1]); // print from 0 -> 4(n)
+    }
+};
