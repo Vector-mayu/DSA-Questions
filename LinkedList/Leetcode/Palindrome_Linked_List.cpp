5ab4e344240ceda5b71041672baa011dbc194c40
Vector-mayu <mayureshdandekar143@gmail.com>
2026-05-21 14:06:18 +0530

    Solved LC 125. Valid Palindrome Problem

--- LinkedList/Leetcode/Palindrome_Linked_List.cpp
+++ LinkedList/Leetcode/Palindrome_Linked_List.cpp
@@ -38,3 +38,54 @@ public:
         return true;
     }
 };
+
+
+// Optimized Solution 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if(!head || !head->next)
+        return true;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode *curr = slow;
+        ListNode *prev = NULL;
+
+        while(curr){
+            ListNode *fut = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = fut;
+        }
+
+        ListNode *start = head;
+        ListNode *end = prev;
+
+        while(end){
+            if(start->val != end->val)
+            return false;
+
+            start = start->next;
+            end = end->next;
+        }
+
+        return true;
+    }
+};
