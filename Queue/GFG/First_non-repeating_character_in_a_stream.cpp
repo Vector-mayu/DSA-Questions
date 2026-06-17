f54132c57f5356f053a01bcd61c80f9ac8824f25
Vector-mayu <mayureshdandekar143@gmail.com>
2026-06-17 19:43:31 +0530

    Solved First non-repeating character in a stream

---
+++ Queue/GFG/First_non-repeating_character_in_a_stream.cpp
@@ -0,0 +1,21 @@
+string firstNonRepeating(string A) {
+    queue<char> q;
+    vector<int> freq(26, 0);
+    string ans;
+
+    for (char ch : A) {
+        freq[ch - 'a']++;
+        q.push(ch);
+
+        while (!q.empty() && freq[q.front() - 'a'] > 1) {
+            q.pop();
+        }
+
+        if (q.empty())
+            ans += '#';
+        else
+            ans += q.front();
+    }
+
+    return ans;
+}
