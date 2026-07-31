f795f36338aa668b8c3113040492975e9e4ebb40
Vector-mayu <mayureshdandekar143@gmail.com>
2026-07-31 14:25:59 +0530

    Added Deletion of heap code and heapify opearation

--- Heap/Heap_Insertion.cpp
+++ Heap/Heap_Insertion.cpp
@@ -14,6 +14,7 @@ class MaxHeap{
     }
     
     void insert(int val){
+        // if its the new element
         if(size == index){
             cout<<"Heap Overflow...";
             return;
@@ -39,9 +40,56 @@ class MaxHeap{
         
         cout<<arr[i]<<" Inserted into heap..."<<endl;
     }
+
+    void Heapify(int i){
+        // largest points the largest number across all 3 nums
+        int largest = i;
+        int left = 2*i + 1;
+        int right = 2*i + 2;
+
+        // left < index means that we are not going out of bound
+        if(left < index && arr[largest] < arr[left]){
+            largest = left;
+        }
+        if(right < index && arr[largest] < arr[right]){
+            largest = right;
+        }
+        
+        // if there exists any largest in left or right then only swap
+        if(largest != i){
+            swap(arr[largest], arr[i]);
+            Heapify(largest);
+        }
+    }
+
+    void deleteHeap(){
+        // if heap is empty
+        if(index == 0){
+            cout<<"Heap underflow...";
+            return;
+        }
+
+        // if heap has only one element
+        if(index == 1){
+            cout<<arr[0]<<" is Deleted..."<<endl;
+            index--;
+            return;
+        }
+
+        // we only delete top element int the heap
+        // if we have more than 1 element
+        // we replace last element with 1st element 
+        cout<<arr[0]<<" is Deleted...";
+        arr[0] = arr[index-1];
+        index--;
+
+        // now we will find the right position for that swapped element
+        Heapify(0);
+
+    }
     
     void printHeap(){
-        for(int i=0; i<size; i++){
+        for(int i=0; i<index; i++){
             cout<<arr[i]<<" ";
         }
         cout<<endl;
@@ -55,4 +103,6 @@ int main(){
     h1.insert(50);
     h1.printHeap();
     h1.insert(41);
+    h1.deleteHeap();
+    h1.printHeap();
 }
