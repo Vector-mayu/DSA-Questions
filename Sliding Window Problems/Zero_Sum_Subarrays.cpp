efcada0545a761764cddd914eebc601f871776bd
Vector-mayu <mayureshdandekar143@gmail.com>
2026-08-15 15:03:42 +0530

    Solved GFG zero sum subarray

--- Sliding Window Problems/Zero_Sum_Subarrays.cpp
+++ Sliding Window Problems/Zero_Sum_Subarrays.cpp
@@ -1,32 +1,22 @@
 class Solution {
   public:
     int findSubarray(vector<int> &arr) {
-        // code here.
+        // code here
+        int prefixSum = 0;
+        int result = 0;
+        unordered_map<int, int>count;
+        // edge case -> initially push zero if arr[0] = 0
+        count[0]++;
         
-        long long int total = 0; 
-        
-        unordered_map<long long int, long long int>m;
-        m[0]=1; // Intialize the map with ('0'->1)
-        
-        long long PrefixSum = 0;
-        
-        for(int i=0;i<arr.size();i++)
-        {
-            // Aate the Add kardo
-            PrefixSum+= arr[i];
+        for(int i=0; i<arr.size(); i++){
+            prefixSum += arr[i];
             
-            if(m.count(PrefixSum)) // if number is repeated
-            {
-                total+= m[PrefixSum]; // add count of repeatition of number  
-                m[PrefixSum]++; // increase the count
-            }
-            else // if number is not repeated
-            {
-                m[PrefixSum] = 1; // map me Prefixsum banao and assign count = 1
-            }
+            result += count[prefixSum]; // if not exist it return 0
+            
+            count[prefixSum]++;
         }
         
-        return total;
+        return result;
         
     }
 };
