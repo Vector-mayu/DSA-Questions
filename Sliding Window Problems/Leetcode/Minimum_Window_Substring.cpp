9c63d8e6e749e494e6af60f6840ab2a1c9f20030
Vector-mayu <mayureshdandekar143@gmail.com>
2026-08-23 16:13:56 +0530

    (Optimization) LC 76. Minimum Window Substring using vector method

--- Sliding Window Problems/Leetcode/Minimum_Window_Substring.cpp
+++ Sliding Window Problems/Leetcode/Minimum_Window_Substring.cpp
@@ -70,3 +70,61 @@ public:
         return s.substr(range.first, minRange);
     }
 };
+
+// Approach 2 :- vector method 
+// here finding ant element is O(1) in worst case also
+
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int n = s.size(), target = t.size();
+
+        // edge case || base case
+        if(n < target)
+        return "";
+
+        vector<int>targetCounter(128, 0);
+        
+        // stored t strings all characters and its frequency
+        for(auto c : t){
+            targetCounter[c]++;
+        }
+
+        vector<int>windowCounter(128, 0);
+        int minSize = INT_MAX, left = 0, right = 0;
+        int startIndex = 0; // its is the first index of min window containing minSize of all elements
+        int currTarget = 0;
+
+        while(right < n){
+            char c = s[right];
+            windowCounter[c]++;
+
+            // if that char exist in target and is lesser or equal then only increase the currTarget
+            if(targetCounter[c] && windowCounter[c] <= targetCounter[c]){
+                currTarget++;
+            }
+
+            // if we get all chars in that window
+            while(currTarget == target){
+                // update the min size
+                if(right - left + 1 < minSize){
+                    minSize = right - left + 1;
+                    startIndex = left;
+                }
+
+                // if char's count is <= target counter value
+                // that means now the value is going to decrease
+                char a = s[left]; 
+                if(targetCounter[a] && windowCounter[a] <= targetCounter[a]){
+                    currTarget--;
+                }
+
+                windowCounter[a]--;
+                left++;
+            }
+            right++;
+        }
+
+        return minSize == INT_MAX ? "" : s.substr(startIndex, minSize);
+    }
+};
