f9461c7e61b435a01dabe205d46502adf5d9445c
Vector-mayu <mayureshdandekar143@gmail.com>
2026-08-30 23:56:21 +0530

    Optimized DFS algorithm

--- Graphs/DFS_Traversal.cpp
+++ Graphs/DFS_Traversal.cpp
@@ -1,27 +1,26 @@
-// Time Complexity :- O(V + E)
-// Space Complexity :- O(V)
-
 class Solution {
   public:
   
-  void DFStraversal(int node, vector<vector<int>>&adj, vector<int>&ans, vector<bool>&Visited){
-      Visited[node] = 1;
-      ans.push_back(node);
-      
-      for(int j=0; j<adj[node].size(); j++){
-          if(!Visited[adj[node][j]]){
-            DFStraversal(adj[node][j], adj, ans, Visited);
-          }
-      }
-  }
+    void DFS(vector<vector<int>>&adj, int node, vector<bool>&visited, vector<int>&ans){
+        
+        visited[node] = 1;
+        ans.push_back(node);
+        
+        for(int neighbour : adj[node]){
+            if(!visited[neighbour]){
+                DFS(adj, neighbour, visited, ans);
+            }
+        }
+    }
   
     vector<int> dfs(vector<vector<int>>& adj) {
         // Code here
         int v = adj.size();
-        vector<int>ans;
-        vector<bool>Visited(v, 0);
         
-        DFStraversal(0, adj, ans, Visited);
+        vector<bool>visited(v, 0);
+        vector<int>ans;
+        
+        DFS(adj, 0, visited, ans);
         
         return ans;
     }
